63f3386ae1d59d24a053ee8aa659a919f9609a91
yogeshsahu0582 <yogeshsahu0582@gmail.com>
2024-04-20 17:12:22 +0530

    Initial commit

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+num = int(input("Enter any number :"))
+n1, n2 = 0, 1
+sum = 0
+if num <= 0:
+    print('Please enter number greater than 0')
+else:
+    for i in range(0, num):
+        print(sum, end=" ")
+        n1 = n2
+        n2 = sum
+        sum = n1 + n2
+
+print('completed..')
